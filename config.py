a0e53b544030163864da930a48daba793d2e09fa
nrecover4587-rgb <nrecover4587@gmail.com>
2026-01-16 14:26:09 +0530

    Update START_IMG_URL to new default image

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/epd7p5.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/qqmw3v.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/epd7p5.jpg"
